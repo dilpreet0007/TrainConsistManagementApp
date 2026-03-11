20d404b65d8a7a88dfbaab41afd0090d0ab888f5
dilpreet0007 <dilpreet.sandhu2004@gmail.com>
2026-03-11 10:02:11 +0530

    Performance Comparison (Loops vs Streams)

--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -3,16 +3,17 @@ package com.main;
 import java.util.*;
 import java.util.stream.Collectors;
 
+import com.bogie.Bogie;
 import com.bogie.CargoBogie;
 
 /*
- * UC12: Safety Compliance Check for Goods Bogies
- * 		 Used Stream allMatch to check Validation
- * 		 Validation checking train is same or not
- * 		 Cylindrical only petroleum condition is used
+ * UC13: Performance Comparison (Loops vs Streams)
+ * 		 Loop startTime and endTime is Calculated
+ * 		 Stream startTime and endTime is Calculated
+ * 		 System.nanoTime() is used
  * 		 
  * @author Dilpreet
- * @version 12.0
+ * @version 13.0
  */
 
 public class Main {
@@ -23,32 +24,29 @@ public class Main {
 		System.out.println("==============================");
 		System.out.println();
 		
-		List<CargoBogie> cargoBogie = new ArrayList<>();
-		
-		cargoBogie.add(new CargoBogie("Cylindrical","Petroleum"));
-		cargoBogie.add(new CargoBogie("Open","Coal"));
-		cargoBogie.add(new CargoBogie("Box","Grain"));
-		cargoBogie.add(new CargoBogie("Cylindrical","Coal"));
-		
-		System.out.println("Goods Bogies in Train: ");
-		for(CargoBogie b : cargoBogie) {
-			System.out.println(b.type + " -> " + b.cargo);
-		}
-		System.out.println();
-		
-		boolean isSafe = cargoBogie.stream().allMatch(bogie -> validateBogie(bogie));
-		
-		System.out.println("Safety Compliance Status: " + isSafe);
-		
-		if(isSafe) System.out.println("Train formation is SAFE");
-		else System.out.println("Train formation is NOT SAFE");
+		List<Bogie> bogies = new ArrayList<>();
 
+		bogies.add(new Bogie("Sleeper",72));
+		bogies.add(new Bogie("AC Chair",56));
+		bogies.add(new Bogie("First Class",24));
+		bogies.add(new Bogie("General",90));
+
+		long startTime1 = System.nanoTime();
+		List<Bogie> list1 = bogies.stream().filter(bogie -> bogie.capacity > 60).collect(Collectors.toList());
+		long endTime1 = System.nanoTime();
+		
+		List<Bogie> list2 = new ArrayList<>();
+		long startTime2 = System.nanoTime();
+		for(Bogie bogie : bogies) {
+			if(bogie.capacity > 60) {
+				list2.add(bogie);
+			}
+		}
+		long endTime2 = System.nanoTime();
+		
+		
+		System.out.println("Stream Execution Time(ns): " + (endTime1-startTime1));
+		System.out.println("Loop Execution Time(ns): " + (endTime2-startTime2));
 	}
-	public static boolean validateBogie(CargoBogie b) {
-        if ("cylindrical".equalsIgnoreCase(b.type)) {
-            return "Petroleum".equalsIgnoreCase(b.cargo);
-        }
-        return true;
-    }
 
 }
