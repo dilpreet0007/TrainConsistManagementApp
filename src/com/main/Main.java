a2578e7492ef7dbfb827f80e6b9db4ad2ee71ff6
dilpreet0007 <dilpreet.sandhu2004@gmail.com>
2026-03-11 21:47:22 +0530

    Linear Search for Bogie ID (Array-Based Searching)

--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -4,15 +4,16 @@ import java.util.*;
 
 
 /*
- * UC17: Sort Bogie Names Using Arrays.sort()
- * 		 User provides bogie type names.
-		 System calls Arrays.sort() on the array.
-		 Java internally sorts the values.
-		 Sorted bogie names are displayed.
-		 Program continues.
+ * UC18: Linear Search for Bogie ID (Array-Based Searching)
+ * 		 User provides a list of bogie IDs.
+		 User provides a search key.
+		 System traverses the array sequentially.
+		 Each element is compared with the search key.
+		 If match found, search stops.
+		 Result is displayed.
  * 		 
  * @author Dilpreet
- * @version 17.0
+ * @version 18.0
  */
 
 public class Main {
@@ -23,14 +24,18 @@ public class Main {
 		System.out.println("==============================");
 		System.out.println();
 
-		String[] capacities = {"Sleeper","AC Chair","First Class","General","Luxury"};
+		String[] bogies = {"BG101","BG102","BG103","BG104","BG105"};
 		
-		System.out.println("Original Bogies:");
-		System.out.print(Arrays.toString(capacities) + "\n\n");
+		System.out.println("Available Bogie IDs:");
+		for(String s : bogies) System.out.println(s);
 		
-		Arrays.sort(capacities);
+		String searchID = "BG103";
+		boolean found = false;
 		
-		System.out.println("Sorted Bogies: ");
-		System.out.print(Arrays.toString(capacities));
+		for(String s : bogies) if(s.equals(searchID)) { found = true; break;}
+		System.out.println();
+		
+		if(found) System.out.println("Bogie " + searchID + " found in train consist");
+		else System.out.println("Bogie " + searchID + " not found in train consist");
 	}
 }
